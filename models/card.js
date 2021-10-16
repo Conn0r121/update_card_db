d91c494b5264978a02f757031c530626281374a4
Connor Robinson <connorrobinson121@gmail.com>
2021-10-16 17:52:05 -0400

    readding index and card model

---
+++ models/card.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const cardSchema = new mongoose.Schema({
+    name: {type: String, unique: true},
+    mana_cost: {type: String},
+    cmc: {type: Number},
+    type_line: {type: String},
+    oracle_text: {type: String},
+    colors: {type: [String]}
+});
+
+const cardModel = mongoose.model('Card', cardSchema);
+
+export default cardModel;
